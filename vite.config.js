52d1b0db7901bcd92b920d1f24fd0027fbccd5b3
ShaikhMujahid-7080 <shaikhmujahid7080@gmail.com>
2025-10-28 21:38:09 +0530

    vite.config edit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Dairy-Nearby/',  // 👈 Add this line — must match your repo name exactly
+  base: process.env.NODE_ENV === 'production' ? '/Dairy-Nearby/' : '/',
+  build: {
+    outDir: 'dist',
+  }
 })
